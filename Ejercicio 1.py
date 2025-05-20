750cf05e89b2ab4acf46baa9981abd3b9a831a14
Danyluxelpro <danyluxelpro@gmail.com>
2025-05-20 03:51:27 -0500

    final

--- Ejercicio 1.py
+++ Ejercicio 1.py
@@ -1,26 +1,31 @@
 import numpy as np
 from numpy import random as rd
 import math
-from scipy.stats import logistic
+from scipy.stats import logistic,chi2
 from scipy.stats import chisquare
 from matplotlib import pyplot as plt
+from scipy.stats import kstest
+import statistics
+#INFORMACIÓN
 mods=["Logi"]
 theta=[0]
 tama=[50,100,200,500,1000]
 data={}
 estim=["-2ln","Wn","Rn"]
-
+tam=1000
+datoschi=chi2.rvs(df=1,size=tam)
+#Se definen las densidades y la recursión de NR
 def flog(a,xi):
     res=(np.exp(xi-a)/(1+np.exp(xi-a))**2)
     return res
-
 def fun(a,dat):
+
     dlog=sum((np.exp(y-a)-1)/(1+np.exp(y-a)) for y in dat)
     d2log=-2*sum((np.exp(y-a))/((1+np.exp(y-a))**2) for y in dat)
     fin=a-dlog/d2log
     return fin
 
-
+#Se generan los datos y se calculan lo estimadores
 for th in theta:
     for i in tama:
         data["Log"+str(th)+ " "+ str(i)+" MLE"]=[]
@@ -28,15 +33,18 @@ for th in theta:
         data["Log"+str(th)+ " "+ str(i)+" Wn"]=[]
         data["Log"+str(th)+ " "+ str(i)+" Rn"]=[]
         j=0
-        while j<100:
+        NR=[]
+        #Newton Raphson con valor inicial el promedio
+        while j<tam:
             x=logistic.rvs(size=i)
             ave=sum(x)/i
             fave=fun(ave,x)
             m=0
-            while abs(ave-fave)>=10**(-8) and m<=100:
+            while abs(ave-fave)>=10**(-5) and m<=100:
                 ave=fave
                 fave=fun(ave,x)
                 m+=1
+            NR.append(m)
             Wn=float(i*(1/3)*fave**2)
             Rn=float((3/i)*(sum((np.exp(y)-1)/(1+np.exp(y)) for y in x))**2)
             lamb=float(-2*sum(np.log(flog(y,0)/flog(y,fave)) for y in x))
@@ -45,14 +53,19 @@ for th in theta:
             data["Log"+str(th)+ " "+ str(i)+" -2ln"].append(lamb)
             data["Log"+str(th)+ " "+ str(i)+" Wn"].append(Wn)
             data["Log"+str(th)+ " "+ str(i)+" Rn"].append(Rn)
-            print(j)
+            
             j+=1
     
 
-
+    
+#Pruebas de Pearson. Se hacen 7 celdas con las probabilidades de una chi2 de 6 grados de libertad 
+print("Prueba Pearson 7 celdas")
 quant=[0.032404,0.134029,0.32029,0.62669,1.1397,2.1469]
-for i in tama:
-    for est in estim:
+for est in estim:
+    print("Estimador:"+est)
+    for i in tama:
+        print("Tamaño:"+str(i))
+    
         Y=[0,0,0,0,0,0,0]
         for l in data["Log"+str(th)+ " "+ str(i)+" "+est]:
             if l<=quant[0]:
@@ -69,12 +82,29 @@ for i in tama:
                 Y[5]+=1
             elif quant[5]<l:
                 Y[6]+=1
-        K=sum((y-i/7)**2/(i/7) for y in Y)
+        K=sum(((y-tam/7)**2)/(tam/7) for y in Y)
         data["Log"+str(th)+ " "+ str(i)+" "+est+" K"]=K
         data["Log"+str(th)+ " "+ str(i)+" "+est+" val"]=Y
-        print("Resultado chi para "+str(th)+ " "+ str(i)+" "+est)
-        print(chisquare(Y,ddof=1))
-
         
+        
+        print("Resultado chi para "+str(th)+ " "+ str(i)+" "+est)
+        print(Y)
+        if K>=12.5918:
+            print("Como K="+str(K)+" es mayor al valor de una chi cuadrado de 6 grados el cual es 12.5918 entonces se rechaza la hipotesis H0")
+        else:
+            print("Como K="+str(K)+" es menor al valor de una chi cuadrado de 6 grados el cual es 12.5918 entonces se acepta la hipotesis H0 ")
+        print(chisquare(Y))
+        
+    print("---------------------------------------------------------")
+
+print("Prueba Kolmogorov smirnof")
+for est in estim:
+    print("Estimador:"+est)
+    for i in tama:
+        print("Tamaño:"+str(i))
+        print(kstest(data["Log"+str(th)+ " "+ str(i)+" "+est],"chi2",(datoschi,1)))
+        print()
+
+
 
      
